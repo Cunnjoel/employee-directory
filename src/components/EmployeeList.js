af6657c86f938f700fca1c9b678f47eb6623e819
Cunnjoel <cunningham.joel84@gmail.com>
2021-06-23 20:28:09 -0500

    updating working filter

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -11,30 +11,29 @@ class EmployeeList extends Component {
 
     componentDidMount() {
         fetch("https://randomuser.me/api/?results=6")
-        .then(res => res.json())
-        .then(
-            (res) => {
-                console.log(res.results)
-                this.setState({
-                    isLoaded: true,
-                    results: res.results
-                }, () => {
-                    // this.sortUser()
-                    this.filterUser()
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: false,
-                    error
-                });
-            }
-        )
+            .then(res => res.json())
+            .then(
+                (res) => {
+                    console.log(res.results)
+                    this.setState({
+                        isLoaded: true,
+                        results: res.results
+                    }, () => {
+                    
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: false,
+                        error
+                    });
+                }
+            )
     }
 
-    sortUser(){
+    sortUser() {
         let sortedUsers = this.state.results;
-        sortedUsers.sort(function(a, b) {
+        sortedUsers.sort(function (a, b) {
             var textA = a.name.last.toUpperCase();
             var textB = b.name.last.toUpperCase();
             return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
@@ -44,14 +43,12 @@ class EmployeeList extends Component {
         })
     }
 
-    filterUser(filterKey){
-        var filterKey = ""
+    filterUser = (event) => {
+        var filterKey = event.target.value
         let filterUsers = this.state.results;
-        
-        filterUsers = filterUsers.filter(function(a) {
-            console.log(a.name.lastt)
-            console.log(a.name.last.toUpperCase().includes(filterKey))
-            return a.name.first.toUpperCase().includes(filterKey)
+
+        filterUsers = filterUsers.filter(function (a) {
+            return a.name.first.toUpperCase().includes(filterKey.toUpperCase())
         })
         console.log(filterUsers);
 
@@ -62,43 +59,44 @@ class EmployeeList extends Component {
 
     render() {
         const { error, isLoaded, results } = this.state;
-        if (error) {    
+        if (error) {
             return <div>Error: {error.message}</div>;
         }
         if (!isLoaded) {
             return <div>Loading...</div>;
-        }else {
+        } else {
             return (
-            <div>
-           <container>
-            <button onClick={() => this.sortUser("1")}>A-Z</button>
-            <button onClick={() => this.sortUser()}>Z-A</button>
-        </container>
-            <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Picture</th>
-                    <th>Last Name</th>
-                    <th>First Name</th>
-                    <th>Gender</th>
-                    <th>DOB</th>
-                    <th>Location</th>
-                  </tr>
-                </thead>
-                <tbody>
-                {results.map(result => (<tr>
-                    <td><img src={result.picture.thumbnail} alt="image"></img></td>
-                    <td>{result.name.last}</td>
-                    <td>{result.name.first}</td>
-                    <td>{result.gender}</td>
-                    <td>{result.dob.date}</td>
-                    <td>{result.location.city}</td>
-                  </tr>)
-                    )}
-                  
-                </tbody>
-              </Table>
-              </div>)
+                <div>
+                    <container>
+                        <button onClick={() => this.sortUser()}>A-Z</button>
+                        <button onClick={() => this.sortUser()}>Z-A</button>
+                    </container>
+                    <input type="text" id="myInput" onChange={this.filterUser} placeholder="Search for last names.." />
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Picture</th>
+                                <th>Last Name</th>
+                                <th>First Name</th>
+                                <th>Gender</th>
+                                <th>DOB</th>
+                                <th>Location</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {results.map(result => (<tr>
+                                <td><img src={result.picture.thumbnail} alt="image"></img></td>
+                                <td>{result.name.last}</td>
+                                <td>{result.name.first}</td>
+                                <td>{result.gender}</td>
+                                <td>{result.dob.date}</td>
+                                <td>{result.location.city}</td>
+                            </tr>)
+                            )}
+
+                        </tbody>
+                    </Table>
+                </div>)
         }
     }
 }
